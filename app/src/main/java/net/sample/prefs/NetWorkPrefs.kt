72efc3f977e062bea0b993324b876ae8e1ef0af4
扑倒末末 <bingo110@126.com>
2017-09-19 10:55:48 +0800

    1:修正框架整体配置问题

--- app/src/main/java/net/sample/prefs/NetWorkPrefs.kt
+++ app/src/main/java/net/sample/prefs/NetWorkPrefs.kt
@@ -10,22 +10,7 @@ object NetWorkPrefs {
     val RAIL_WAY_TRAIN="rail_way_train"
     val WEATHER="weather"
     init {
-        Configuration.init {
-            item {
-                action = RAIL_WAY_TRAIN
-                info = "火车票车次查询"
-                url = "train/tickets/%s/queryByTrainNo?"
-                method=RequestMethod.post
-                params = arrayOf("key", "trainno")
-            }
-            item {
-                action = WEATHER
-                info = "天气查询"
-                url = "v1/weather/query?"
-                params = arrayOf("key", "city","province")
-            }
-
-        }
+        Configuration.register(OtherNetPrefs())
     }
 
 }
--- netlibrary/src/main/java/cz/netlibrary/model/Configuration.kt
+++ netlibrary/src/main/java/cz/netlibrary/model/Configuration.kt
@@ -7,15 +7,11 @@ object Configuration {
 
     val requestItems= mutableListOf<RequestItem>()
 
-    fun init(init:Config.()->Unit)=Config().apply(init)
+    fun register(item:NetPrefsItem){
+        requestItems.addAll(item.requestItems)
+    }
 
     //重载运算符 get 可在使用RequestManager["action"]获取请求条目
     operator fun get(action:String): RequestItem? = requestItems.find { it.action==action }
 
-
-    class Config{
-        fun item(closure:RequestItem.()->Unit){
-            requestItems.add(RequestItem().apply(closure))
-        }
-    }
 }
